1776645ca6aa101771367ccf7194b841e20890ad
karimiyani <front_end2020@yahoo.com>
2021-09-23 20:39:00 +0330

    first commit

---
+++ js/index.js
@@ -0,0 +1,28 @@
+
+//Math.E
+console.log(Math.E);
+//2.718281828459045
+
+
+//Math.pI
+console.log(Math.PI);
+//3.141592653589793
+
+
+//Math.floor()
+const num = 7.5;
+console.log(Math.floor(num));
+//7
+
+//Math.ceil()
+const num2 = 7.6;
+console.log(Math.ceil(num2));
+//8
+
+//Math.round()
+const num3 = 7.3;
+console.log(Math.round(num3));
+//7
+
+
+
